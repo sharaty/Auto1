9ab98766512733760badc3e3440cbed36a4248f7
SHARAT <SHARAT@HP>
2023-02-07 14:28:07 +0530

    build

---
+++ Maven/src/test/java/Scripts/Firefox_Launch.java
@@ -0,0 +1,18 @@
+package Scripts;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Firefox_Launch 
+{
+	@Test
+	public void test () 
+	{
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+	}
+}
+
